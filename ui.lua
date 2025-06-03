4c0adc2a786d4232daa17469f6c446fca19a2438
Axyraandas <62352786+Axyraandas@users.noreply.github.com>
2025-06-03 07:36:44 -0400

    Organized UI file
    Added some newlines, still working on functionality

--- ui.lua
+++ ui.lua
@@ -8,12 +8,14 @@ G.FUNCS.berry_credits_sprite = function(self)
 	-- ret.states.visible = true
 	-- self.config.object.sprite_pos.x = (self.config.object.sprite_pos.x + .01) % 5
 	-- print(target)
-	self.UIBox:get_UIE_by_ID('berry_center_sprite').config.object.x = 0
+	self.UIBox:get_UIE_by_ID('berry_center_sprite').config.object.sprite_pos.x = 0
 	-- return ret
 end
 
 G.FUNCS.tony_hover = function(self)
-	print(G.CONTROLLER.hovering.target.config.id)
+	if G.CONTROLLER.hovering.prev_target ~= G.CONTROLLER.hovering.target then
+		print(G.CONTROLLER.hovering.target.config)
+	end
 	-- print(self.states.hover.is)
 	if self.states.hover.is then
 		target = 1
@@ -30,73 +32,160 @@ SMODS.current_mod.extra_tabs = function()
             tab_definition_function = function()
                 return {
                     n = G.UIT.ROOT,
-                    config = { r = 0.1, minw = 10, minh = 6.5, align = "tm", padding = 0.2, colour = G.C.BLACK, func = 'berry_credits_sprite', id = 'berry_root' },
+                    config = {
+					r = 0.1,
+					minw = 10,
+					minh = 6.5,
+					align = "tm",
+					padding = 0.2,
+					colour = G.C.BLACK,
+					func = 'berry_credits_sprite',
+					id = 'berry_root'
+					},
 					instance_type = 'NODE',
                     nodes = { {
                         n = G.UIT.R,
                         config = { minw = 10, minh = 6 },
                         nodes = { {
                             n = G.UIT.C,
-                            config = { r = 0.1, minw = 3.33, minh = 6, padding = 0.2 },
+                            config = {
+							r = 0.1,
+							minw = 3.33,
+							minh = 6,
+							padding = 0.2
+							},
 							instance_type = 'CARD',
                             nodes = { {
                                 n = G.UIT.R,
-                                config = { r = 0.1, colour = G.C.GREEN, minh = 0.75, minw = 2.8, align = "cm", hover = true, shadow = true },
+                                config = {
+								r = 0.1,
+								colour = G.C.GREEN,
+								minh = 0.75,
+								minw = 2.8,
+								align = "cm",
+								hover = true,
+								shadow = true
+								},
                                 nodes = { {
-                                    n = G.UIT.T, config = { text = "Programmers", colour = G.C.WHITE, scale = 0.5, hover = true, shadow = true }
+                                    n = G.UIT.T, config = {
+									text = "Programmers",
+									colour = G.C.WHITE,
+									scale = 0.5,
+									hover = true,
+									shadow = true
+									}
 
                                 } }
                             },
                                 {
                                     n = G.UIT.R,
                                     nodes = { {
-                                        n = G.UIT.T, config = { text = "TonyKrZa", colour = HEX("0081D6FF"), func = 'tony_hover', emboss = 0.05, scale = 0.35, hover = true, shadow = true }
+                                        n = G.UIT.T, config = {
+										text = "TonyKrZa",
+										colour = HEX("0081D6FF"),
+										emboss = 0.05,
+										scale = 0.35,
+										hover = true,
+										shadow = true,
+										func = 'tony_hover',
+										}
 
                                     } }
                                 },
                                 {
                                     n = G.UIT.R,
                                     nodes = { {
-                                        n = G.UIT.T, config = { text = "Axyraandas", colour = HEX("3E993EFF"), emboss = 0.05, scale = 0.35, hover = true, shadow = true }
+                                        n = G.UIT.T, config = {
+										text = "Axyraandas",
+										colour = HEX("3E993EFF"),
+										emboss = 0.05,
+										scale = 0.35,
+										hover = true,
+										shadow = true
+										}
 
                                     } }
                                 },
                                 {
                                     n = G.UIT.R,
                                     nodes = { {
-                                        n = G.UIT.T, config = { text = "fumiko_239", colour = HEX("6CF5FFFF"), emboss = 0.05, scale = 0.35, hover = true, shadow = true }
+                                        n = G.UIT.T, config = {
+										text = "fumiko_239",
+										colour = HEX("6CF5FFFF"),
+										emboss = 0.05,
+										scale = 0.35,
+										hover = true,
+										shadow = true
+										}
 
                                     } }
                                 },
                                 {
-                                    n = G.UIT.R, config = { minh = 0.5, minw = 3.33 }
+                                    n = G.UIT.R, config = {
+									minh = 0.5,
+									minw = 3.33
+									}
                                 },
                                 {
                                     n = G.UIT.R,
-                                    config = { r = 0.1, colour = G.C.BLUE, minh = 0.75, minw = 2.8, align = "cm", hover = true, shadow = true },
+                                    config = {
+									r = 0.1,
+									colour = G.C.BLUE,
+									minh = 0.75,
+									minw = 2.8,
+									align = "cm",
+									hover = true,
+									shadow = true
+									},
                                     nodes = { {
-                                        n = G.UIT.T, config = { text = "Artists", colour = G.C.WHITE, scale = 0.5, hover = true, shadow = true }
+                                        n = G.UIT.T, config = {
+										text = "Artists",
+										colour = G.C.WHITE,
+										scale = 0.5,
+										hover = true,
+										shadow = true
+										}
 
                                     } }
                                 },
                                 {
                                     n = G.UIT.R,
                                     nodes = { {
-                                        n = G.UIT.T, config = { text = "TonyKrZa", colour = HEX("0081D6"), emboss = 0.05, scale = 0.35, hover = true, shadow = true }
+                                        n = G.UIT.T, config = {
+										text = "TonyKrZa",
+										colour = HEX("0081D6"),
+										emboss = 0.05,
+										scale = 0.35,
+										hover = true,
+										shadow = true
+										}
 
                                     } }
                                 },
                                 {
                                     n = G.UIT.R,
                                     nodes = { {
-                                        n = G.UIT.T, config = { text = "Breadcrumb5550", colour =  HEX("FFA01C"), emboss = 0.05, scale = 0.35, hover = true, shadow = true }
-
+                                        n = G.UIT.T, config = {
+										text = "Breadcrumb5550",
+										colour =  HEX("FFA01C"),
+										emboss = 0.05,
+										scale = 0.35,
+										hover = true,
+										shadow = true
+										}
                                     } }
                                 },
                                 {
                                     n = G.UIT.R,
                                     nodes = { {
-                                        n = G.UIT.T, config = { text = "Quiyuan", colour = HEX("FF84F9"), emboss = 0.05, scale = 0.35, hover = true, shadow = true }
+                                        n = G.UIT.T, config = {
+										text = "Quiyuan",
+										colour = HEX("FF84F9"),
+										emboss = 0.05,
+										scale = 0.35,
+										hover = true,
+										shadow = true
+										}
 
                                     } }
                                 }
@@ -133,72 +222,149 @@ SMODS.current_mod.extra_tabs = function()
                                 config = { r = 0.1, minw = 3.33, minh = 6, padding = 0.2 },
                                 nodes = { {
                                     n = G.UIT.R,
-                                    config = { r = 0.1, colour = G.C.RED, minh = 0.75, minw = 2.8, align = "cm", hover = true, shadow = true },
+                                    config = {
+									r = 0.1,
+									colour = G.C.RED,
+									minh = 0.75,
+									minw = 2.8,
+									align = "cm",
+									hover = true,
+									shadow = true
+									},
                                     nodes = { {
-                                        n = G.UIT.T, config = { text = "Special Thanks", colour = G.C.WHITE, scale = 0.5, hover = true, shadow = true }
+                                        n = G.UIT.T, config = {
+										text = "Special Thanks",
+										colour = G.C.WHITE,
+										scale = 0.5,
+										hover = true,
+										shadow = true
+										}
 
                                     } }
                                 },
                                     {
                                         n = G.UIT.R, config = { align = "cr"},
                                         nodes = { {
-                                            n = G.UIT.T, config = { text = "Koishukaze", colour = HEX("DB8F00"), emboss = 0.05, scale = 0.35, hover = true, shadow = true }
+                                            n = G.UIT.T, config = {
+											text = "Koishukaze",
+											colour = HEX("DB8F00"),
+											emboss = 0.05,
+											scale = 0.35,
+											hover = true,
+											shadow = true
+											}
 
                                         } }
                                     },
                                     {
                                         n = G.UIT.R, config = { align = "cr"},
                                         nodes = { {
-                                            n = G.UIT.T, config = { text = "stickmancomic", colour = HEX("FF5555"), emboss = 0.05, scale = 0.35, hover = true, shadow = true }
+                                            n = G.UIT.T, config = {
+											text = "stickmancomic",
+											colour = HEX("FF5555"),
+											emboss = 0.05,
+											scale = 0.35,
+											hover = true,
+											shadow = true
+											}
 
                                         } }
                                     },
                                     {
                                         n = G.UIT.R, config = { align = "cr"},
                                         nodes = { {
-                                            n = G.UIT.T, config = { text = "zqr_", colour = HEX("5f7377"), emboss = 0.05, scale = 0.35, hover = true, shadow = true }
+                                            n = G.UIT.T, config = {
+											text = "zqr_",
+											colour = HEX("5f7377"),
+											emboss = 0.05,
+											scale = 0.35,
+											hover = true,
+											shadow = true
+											}
 
                                         } }
                                     },
                                     {
                                         n = G.UIT.R, config = { align = "cr"},
                                         nodes = { {
-                                            n = G.UIT.T, config = { text = "Lifeline / Messy Bookshelf", colour = HEX("FFD900"), emboss = 0.05, scale = 0.35, hover = true, shadow = true }
+                                            n = G.UIT.T, config = {
+											text = "Lifeline / Messy Bookshelf", 
+											colour = HEX("FFD900"),
+											emboss = 0.05,
+											scale = 0.35,
+											hover = true,
+											shadow = true
+											}
 
                                         } }
                                     },
                                     {
                                         n = G.UIT.R, config = { align = "cr"},
                                         nodes = { {
-                                            n = G.UIT.T, config = { text = "Bentux", colour = HEX("3CFF00"), emboss = 0.05, scale = 0.35, hover = true, shadow = true }
+                                            n = G.UIT.T, config = {
+											text = "Bentux",
+											colour = HEX("3CFF00"),
+											emboss = 0.05,
+											scale = 0.35,
+											hover = true,
+											shadow = true
+											}
 
                                         } }
                                     },
                                     {
                                         n = G.UIT.R, config = { align = "cr"},
                                         nodes = { {
-                                            n = G.UIT.T, config = { text = "Hanya", colour = G.C.WHITE, emboss = 0.05, scale = 0.35, hover = true, shadow = true }
+                                            n = G.UIT.T, config = {
+											text = "Hanya",
+											colour = G.C.WHITE,
+											emboss = 0.05,
+											scale = 0.35,
+											hover = true,
+											shadow = true
+											}
 
                                         } }
                                     },
                                     {
                                         n = G.UIT.R, config = { align = "cr"},
                                         nodes = { {
-                                            n = G.UIT.T, config = { text = "The Balatro Discords", colour = HEX("9700BD"), emboss = 0.05, scale = 0.35, hover = true, shadow = true }
+                                            n = G.UIT.T, config = {
+											text = "The Balatro Discords",
+											colour = HEX("9700BD"),
+											emboss = 0.05,
+											scale = 0.35,
+											hover = true,
+											shadow = true
+											}
 
                                         } }
                                     },
                                     {
                                         n = G.UIT.R, config = { align = "cr"},
                                         nodes = { {
-                                            n = G.UIT.T, config = { text = "...and you!", colour = G.C.DARK_EDITION, emboss = 0.05, scale = 0.35, hover = true, shadow = true }
+                                            n = G.UIT.T, config = {
+											text = "...and you!",
+											colour = G.C.DARK_EDITION,
+											emboss = 0.05,
+											scale = 0.35,
+											hover = true,
+											shadow = true
+											}
 
                                         } }
                                     },
                                     {
                                         n = G.UIT.R, config = { align = "cr"},
                                         nodes = { {
-                                            n = G.UIT.T, config = { text = "(...and more to come!)", colour = HEX("FFFFFF40"), emboss = 0.05, scale = 0.35, hover = true, shadow = true }
+                                            n = G.UIT.T, config = {
+											text = "(...and more to come!)",
+											colour = HEX("FFFFFF40"),
+											emboss = 0.05,
+											scale = 0.35,
+											hover = true,
+											shadow = true
+											}
 
                                         } }
                                     }
